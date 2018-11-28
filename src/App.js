dc51d76e4b32bfc2b72cea7c27d39759be6f9a0a
Kento Koumura <milkmeta@gmail.com>
2018-11-28 17:18:04 +0900

    サンプルデータを充実させる

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends MicroContainer {
       master: {
         root: {
           title: 'Root',
+          starred: true,
           children: [
             'task01',
             'task02',
@@ -19,23 +20,65 @@ class App extends MicroContainer {
           ]
         },
         task01: {
-          title: 'Task 1',
+          title: 'Task 01',
           date: '2019-01-01',
-          starred: true
+          children: [
+            'task01_01',
+            'task01_02'
+          ]
+        },
+        task01_01: {
+          title: 'Task 01_01',
+          date: '2019-01-01'
+        },
+        task01_02: {
+          title: 'Task 01_02',
+          children: [
+            'task02_01',
+            'task02_02'
+          ]
         },
         task02: {
-          title: 'Task 2',
-          date: '2019-02-01',
-          checked: true
+          title: 'Task 02',
+          starred: true
+        },
+        task02_01: {
+          title: 'Task 02_01',
+          starred: true
+        },
+        task02_02: {
+          title: 'Task 02_02'
         },
         task03: {
-          title: 'Task 3',
-          date: '2019-03-01'
+          title: 'Task 03',
+          checked: true,
+          children: [
+            'task03_01',
+            'task03_02'
+          ]
+        },
+        task03_01: {
+          title: 'Task 03_01',
+          checked: true
+        },
+        task03_02: {
+          title: 'Task 03_02'
         },
         task04: {
-          title: 'Task 4',
+          title: 'Task 04',
           date: '2019-04-01',
+          archived: true,
+          children: [
+            'task04_01',
+            'task04_02'
+          ]
+        },
+        task04_01: {
+          title: 'Task 04_01',
           archived: true
+        },
+        task04_02: {
+          title: 'Task 04_02'
         }
       }
     };
