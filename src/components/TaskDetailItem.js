f64199823c95c3afa52932feb7d2b68211830727
Kento Koumura <milkmeta@gmail.com>
2018-11-28 14:11:06 +0900

    タスク詳細アイテムの内容充実

--- src/components/TaskDetailItem.js
+++ src/components/TaskDetailItem.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './TaskDetailItem.css';
 
 const TaskDetailItem = props =>  (
-  <div className="TaskDetailItem">{props.title}</div>
+  <div className="TaskDetailItem">
+    <input type="text" className="TaskDetailItem__title" defaultValue={props.title} />
+    <input type="text" className="TaskDetailItem__date" defaultValue={props.date} />
+    <button>menu</button>
+  </div>
 );
 
 export default TaskDetailItem;
--- src/components/TaskDetailPanel.js
+++ src/components/TaskDetailPanel.js
@@ -5,9 +5,9 @@ import TaskDetailItem from './TaskDetailItem';
 const TaskDetailPanel = props =>  (
   <div className="TaskDetailPanel">
     <ul className="TaskDetailPanel__list">
-      <TaskDetailItem title="あいうえお" />
-      <TaskDetailItem title="かきくけこ" />
-      <TaskDetailItem title="さしすせそ" />
+      <TaskDetailItem title="あいうえお" date="01/01" />
+      <TaskDetailItem title="かきくけこ" date="02/01" />
+      <TaskDetailItem title="さしすせそ" date="03/01" />
     </ul>
   </div>
 );
