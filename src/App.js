77ad231efd10ed105698a5c9bdb154d482e70d09
Kento Koumura <milkmeta@gmail.com>
2018-11-28 11:47:56 +0900

    react-micro-containerのサンプルを参考にコンテナの作成

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import MicroContainer from 'react-micro-container';
 import uuidv4 from 'uuid/v4';
 import Main from './components/Main';
 
-class App extends Component {
+class App extends MicroContainer {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
     return (
-      <Main />
+      <Main dispatch={this.dispatch} {...this.state} />
     );
   }
 }
