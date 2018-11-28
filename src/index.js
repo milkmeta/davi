544aec9a2ce68ec4ea158db58b83b0b636695b3e
Kento Koumura <milkmeta@gmail.com>
2018-11-28 11:55:01 +0900

    importの順序をCSS→JSの順に変更

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import 'sanitize.css';
 import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
