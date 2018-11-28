8b32aef96dff3cccd036c71ba870ba698f74a37c
Kento Koumura <milkmeta@gmail.com>
2018-11-28 11:51:25 +0900

    プロジェクト名を各ファイルに反映

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './Main.css';
 
 const Main = props =>  (
-  <div className="Main"></div>
+  <div className="Main">
+    <h1>Davi</h1>
+  </div>
 );
 
 export default Main;
