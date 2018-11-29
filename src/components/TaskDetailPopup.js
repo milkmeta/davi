95d20dbde885a7cd043a36d1ce1cee526657cace
Kento Koumura <milkmeta@gmail.com>
2018-11-29 14:28:53 +0900

    ポップアップメニュー コードをより見通しよく調整

--- src/components/TaskDetailPopup.js
+++ src/components/TaskDetailPopup.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './TaskDetailPopup.scss';
 
+const popupName = 'TaskDetailPopup';
+
 const TaskDetailPopup = props => (
-  <ul className="TaskDetailPopup" data-visible={props.display.name === 'todoDetailPopup' && props.display.show} style={{
+  <ul className="TaskDetailPopup" data-visible={props.display.name === popupName && props.display.show} style={{
     left: props.display.pageX,
     top: props.display.pageY
   }}>
     <li><button onClick={e => {
       props.dispatch('todoChangeStar', props.display.id);
-      props.dispatch('todoPopup', props.display.id, 'todoDetailPopup', e)
+      props.dispatch('todoPopup', props.display.id, popupName, e)
     }}>スター</button></li>
     <li><button onClick={e => {
       props.dispatch('todoChangeArchive', props.display.id);
-      props.dispatch('todoPopup', props.display.id, 'todoDetailPopup', e)
+      props.dispatch('todoPopup', props.display.id, popupName, e)
     }}>アーカイブ</button></li>
   </ul>
 );
--- src/components/TaskSummaryPopup.js
+++ src/components/TaskSummaryPopup.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './TaskSummaryPopup.scss';
 
+const popupName = 'TaskSummaryPopup';
+
 const TaskSummaryPopup = props => (
-  <ul className="TaskSummaryPopup" data-visible={props.display.name === 'todoSummaryPopup' && props.display.show} style={{
+  <ul className="TaskSummaryPopup" data-visible={props.display.name === popupName && props.display.show} style={{
     left: props.display.pageX,
     top: props.display.pageY
   }}>
     <li><button onClick={e => {
       props.dispatch('todoChangeStar', props.display.id);
-      props.dispatch('todoPopup', props.display.id, 'todoSummaryPopup', e)
+      props.dispatch('todoPopup', props.display.id, popupName, e)
     }}>スター</button></li>
     <li><button onClick={e => {
       props.dispatch('todoChangeArchive', props.display.id);
-      props.dispatch('todoPopup', props.display.id, 'todoSummaryPopup', e)
+      props.dispatch('todoPopup', props.display.id, popupName, e)
     }}>アーカイブ</button></li>
   </ul>
 );
