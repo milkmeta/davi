a1f91d58da3aca5522b11496543eb02e41479b28
Kento Koumura <milkmeta@gmail.com>
2018-11-29 11:27:33 +0900

    各タスクのデフォルト値をまとめて設定しコードを見やすく

--- src/components/TaskDetailItem.js
+++ src/components/TaskDetailItem.js
@@ -4,10 +4,16 @@ import TaskDetailList from './TaskDetailList';
 import './TaskDetailItem.scss';
 
 const TaskDetailItem = props => {
-  const item = props.master[props.id];
+  const itemDefault = {
+    title: '',
+    date: ''
+  }
+  const itemRaw = props.master[props.id];
+  const item = Object.assign(itemDefault, itemRaw)
   const itemInfo = {
     id: props.id
   };
+
   if (item.archived) {
     return false;
   }
@@ -15,8 +21,8 @@ const TaskDetailItem = props => {
     <div className="TaskDetailItem" data-starred={item.starred} data-checked={item.checked}>
       <div className="TaskDetailItem__self">
         <input type="checkbox" className="TaskDetailItem__checkbox" defaultChecked={item.checked} />
-        <input type="text" className="TaskDetailItem__title" value={typeof item.title !== 'undefined' ? item.title : ''} onChange={e => props.dispatch('todoChangeTitle', itemInfo, e.target.value)} />
-        <input type="text" className="TaskDetailItem__date" value={typeof item.date !== 'undefined' ? item.date : ''} onChange={e => props.dispatch('todoChangeDate', itemInfo, e.target.value)} />
+        <input type="text" className="TaskDetailItem__title" value={item.title} onChange={e => props.dispatch('todoChangeTitle', itemInfo, e.target.value)} />
+        <input type="text" className="TaskDetailItem__date" value={item.date} onChange={e => props.dispatch('todoChangeDate', itemInfo, e.target.value)} />
         <button className="TaskDetailItem__button"><FontAwesomeIcon icon={['fas', 'ellipsis-h']} /></button>
       </div>
       <div className="TaskDetailItem__children">
