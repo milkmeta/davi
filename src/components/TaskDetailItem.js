0868ff5e34262d94bbfd6cbe832bee1b8dbcc360
Kento Koumura <milkmeta@gmail.com>
2018-11-30 15:38:58 +0900

    詳細タスクの表示をスマホも考慮に調整

--- src/components/TaskDetailItem.js
+++ src/components/TaskDetailItem.js
@@ -21,8 +21,10 @@ const TaskDetailItem = props => {
     <div className="TaskDetailItem" data-starred={item.starred} data-checked={item.checked}>
       <div className="TaskDetailItem__self">
         <input type="checkbox" className="TaskDetailItem__checkbox" checked={item.checked} onChange={() => props.dispatch('todoChangeStatus', props.id)} />
-        <input type="text" className="TaskDetailItem__title" value={item.title} onChange={e => props.dispatch('todoChangeTitle', props.id, e.target.value)} />
-        <input type="date" className="TaskDetailItem__date" value={item.date} onChange={e => props.dispatch('todoChangeDate', props.id, e.target.value)} />
+        <div className="TaskDetailItem__texts">
+          <input type="text" className="TaskDetailItem__text TaskDetailItem__text--title" value={item.title} onChange={e => props.dispatch('todoChangeTitle', props.id, e.target.value)} />
+          <input type="date" className="TaskDetailItem__text TaskDetailItem__text--date" value={item.date} onChange={e => props.dispatch('todoChangeDate', props.id, e.target.value)} />
+        </div>
         <button className="TaskDetailItem__button" onClick={e => props.dispatch('todoPopup', 'TaskDetailPopup', props.id, e)}><FontAwesomeIcon icon={['fas', 'ellipsis-h']} /></button>
       </div>
       <div className="TaskDetailItem__children">
